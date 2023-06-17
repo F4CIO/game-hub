d8a7c10dace80866b9f05f038bcdf4efd59f1d7c
ncurcin <ncurcin@craftsynth.com>
2023-06-17 17:28:29 +0200

    create custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games1")
-      .then((r) => {
-        setGames(r.data.results);
-      })
-      .catch((r) => setError(r.message));
-  });
+  const { games, error } = useGames();
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
-        {games && games.map((game) => <li key={game.id}>{game.name}</li>)}
-      </ul>
+      <ul>{games && games.map((g) => <li key={g.id}>{g.name}</li>)}</ul>
     </>
   );
 };
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://api.rawg.io/api/games',
+    baseURL:'https://api.rawg.io/api/',
     params:{
         key:'c7bb5a4db6e24f828291723c5b898b2f'
     }
