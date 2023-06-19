c3907428f8164d3b5ea5f3b0d1b728349d502950
ncurcin <ncurcin@craftsynth.com>
2023-06-19 20:31:07 +0200

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/android.jpg";
+
 const getCroppedImageUrl = (url:string) => {
     if(!url){
-        return '';
+        return noImage;
     }else{
         const index = url.indexOf('media/')+'media/'.length;
         return url.slice(0,index)+'crop/600/400/'+url.slice(index);
